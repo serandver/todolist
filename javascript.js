e9ffcbb88f68c4f093fa30293a748001b29bb85c
serg <serg-x@list.ru>
2014-12-04 01:58:31 +0200

    javascript: add newtask, onclick toogleclass

--- javascript.js
+++ javascript.js
@@ -1,25 +1,56 @@
 $(document).ready(function() {
-
-	//default quantity 
-	var arrayCheckbox = $('.task-todo');
-	$('.item span').text(arrayCheckbox.length);
-	$('.completed-item span').text('0');
-
-	//line-through selected item
- 	$('.wrapper-checkbox input').click(function() {
-   		$(this).parent('div').parent('div').children('.task-todo').toggleClass('selected');
+/*
 	
-		//items left to do
-		var arraySelected = $('.selected');	
-		var itemsLeft = arrayCheckbox.length - arraySelected.length;
-  		$('.item span').text(itemsLeft);	
-	
-		//clear items
-  		var clearItem = arraySelected.length;
-		$('.completed-item span').text(clearItem);
-   	});
   	
-  	//editable todo
+*/
+	var myTasks = []; //создаем пустой массив будущих заданий
+	$('.completed-item span').text('0');  //обнуляем счетчики
+	$('.item span').text('0');
+
+
+	$("#add").click (function () {
+		if ($("#newItem").val() !== "") {
+			var e = $("#newItem").val();
+			function newTask (e) {     //создаем конструктор объектов - наших новых заданий
+				this.todo = e;
+				this.status = false;
+			};
+			var myTask = new newTask(e);   //создаем новый объект с значением свойства todo
+			myTasks[myTasks.length] = myTask;  //добавляем новое задание в конец массива
+		} 
+		else {alert("Вы не ввели новое задание!")};
+
+		$("#mytasks").append('<div class="wrapper-task"><div class="wrapper-checkbox"><input type="checkbox"/></div><div class="task-todo">' + e + '</div></div>'); //добавляем задание в конец списка
+		$("#newItem").val("What need to been done?");
+
+		var arrayCheckbox = $('.task-todo');
+		$('.item span').text(arrayCheckbox.length); //счетчик количества заданий
+		$('#mytasks').on("click", "input", function() {
+			$(this).parent('div').parent('div').children('.task-todo').toggleClass('selected'); 
+		})
+		
+
+
+			/*
+			//items left to do
+			var arraySelected = $('.selected');	
+			var itemsLeft = arrayCheckbox.length - arraySelected.length;
+		  	$('.item span').text(itemsLeft);	
+			color: #6e6e6e;
+		font-style: bold; 	text-decoration: line-through;
+			//clear items
+		  	var clearItem = arraySelected.length;
+			$('.completed-item span').text(clearItem);
+			*/
+	})
+
+	    
+	
+//default quantity 
+
+
+//line-through selected item
+
   	
 
 }); //end ready
